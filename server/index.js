a856258d8eb91c394e620893639e7c021bbb074a
JULIANO TAVARES DA SILVA <juliano_t_da-silva@estudante.sesisenai.org.br>
2024-10-22 12:24:47 -0300

    rotas

--- server/index.js
+++ server/index.js
@@ -1,15 +1,49 @@
 import Express from "express";
+import { criarTabelas, User } from "./db.js";
+import bcryptjs from "bcryptjs"
 
 const app = Express()
 app.use(Express.json())
+//criarTabelas()
 
-app.post('/registro', (req, res) => {
+app.post('/registro', async (req, res) => {
     const {nome, sobrenome, email, senha, dataNascimento} = req.body
     if (!nome || !sobrenome || !email || !senha || !dataNascimento){
-        res.send('voce deve completar todos os campos')
+        res.send('Você deve completar todos os campos.')
         return
     }
+    const userExiste = await User.findOne({where: {email: email}})
+    if (userExiste){
+        res.send('Usuario ja existe!')
+        return
+    }
+    const senhaCriptografada = bcryptjs.hashSync(senha, 10)
+
+    const teste = await User.create({nome, sobrenome, email, senha: senhaCriptografada, dataNascimento})
+
+
+
     res.send('Usuário criado!')
+})
+
+app.post('/login', async (req, res) => {
+    const {email, senha} = req.body
+    if (!email || !senha){
+        res.send('Preencha todos os campos.')
+        return
+    }
+    const userExiste = await User.findOne({where: {email: email}})
+    if (!userExiste){
+        res.send('Usuario não existe!')
+        return
+    }
+    const senhaValida = bcryptjs.compareSync(senha, userExiste.senha)
+    if (!senhaValida){
+        res.send('email ou senha invalido!')
+        return
+    }
+
+    res.send('Login efetuado com sucesso!')
 })
 
 app.listen(8000)
